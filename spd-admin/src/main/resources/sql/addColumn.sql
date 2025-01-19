333b1c2567357970dfb7d0254880936652bb4bd5
boduoyejieza <boduoyejieza@gmail.com>
2025-01-19 21:20:21 +0800

    添加单据种子表和高值耗材入库单明细院内码明细表

--- spd-admin/src/main/resources/sql/addColumn.sql
+++ spd-admin/src/main/resources/sql/addColumn.sql
@@ -16,6 +16,8 @@ ALTER TABLE spd.gz_refund_stock ADD secondary_barcode varchar(200) NULL COMMENT 
 ALTER TABLE spd.gz_refund_goods_entry ADD master_barcode varchar(200) NULL COMMENT '主条码';
 ALTER TABLE spd.gz_refund_goods_entry ADD secondary_barcode varchar(200) NULL COMMENT '辅条码';
 
+ALTER TABLE spd.gz_depot_inventory ADD in_hospital_code varchar(200) NULL COMMENT '院内码';
+ALTER TABLE spd.gz_dep_inventory ADD in_hospital_code varchar(200) NULL COMMENT '院内码';
 
 
 
