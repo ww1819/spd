617bb6d045a8d76c110683c0957e517be0270673
boduoyejieza <boduoyejieza@gmail.com>
2025-06-17 10:32:46 +0800

    添加设备标签打印逻辑

--- spd-biz/src/main/java/com/sb/sbInfo/controller/SbInfoController.java
+++ spd-biz/src/main/java/com/sb/sbInfo/controller/SbInfoController.java
@@ -3,6 +3,7 @@ package com.sb.sbInfo.controller;
 import com.sb.sbInfo.service.SbInfoService;
 import com.spd.common.core.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,18 @@ public class SbInfoController extends BaseController {
     @Autowired
     private SbInfoService sbInfoService;
 
-
-
+    /**
+     * 获取设备标签信息
+     *
+     * @param code 设备编码
+     * @return 设备标签信息
+     */
+    @PreAuthorize("@ss.hasPermi('sb:sbInfo:getSbLabelInfo')")
+    @RequestMapping("/getSbLabelInfo")
+    public String getSbLabelInfo(String code) {
+        if (code == null || code.isEmpty()) {
+            return "设备编码不能为空";
+        }
+        return sbInfoService.getSbLabelInfo(code);
+    }
 }
